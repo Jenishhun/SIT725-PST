4ab38f5071d0fd90e32b1f4b6f995d37aa2d394d
prit <pritboghara99@gmail.com>
2025-05-29 11:06:17 +1000

    Made some changes

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -4,8 +4,8 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: 'meetlakhani98787@gmail.com', // Your email
-        pass: 'qyzn modm mnsk atzc'   // Your email password
+        user: 'jenishhun7@gmail.com', // Your email
+        pass: 'rllr vesp fcmc vubu'   // Your email password
     }
 });
 
